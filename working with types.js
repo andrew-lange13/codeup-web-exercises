14ce5556b2742a55f6871f0da667f710b5211cfb
Andrew Lange <andrew.lange13@gmail.com>
2021-11-22 19:16:57 -0600

    adds exercise 3 and 4 to working with types exercise

--- working with types.js
+++ working with types.js
@@ -1,3 +1,5 @@
+// Exercise 1
+
 var a = 1;
 var b = a++;
 var c = ++a;
@@ -89,6 +91,60 @@ console.log(sample);
 console.log(sample.indexOf("c"));
 console.log(sample.indexOf("C"));
 console.log(sample.substring(6, 12));
+
+
+// Exercise 3
+
+var the_little_mermaid = 3, brother_bear = 5, hercules = 1;
+
+console.log((the_little_mermaid * 3) + (brother_bear * 3) + (hercules * 3)  );
+
+
+
+
+
+
+
+var Google = 400, Amazon = 380, Facebook = 350;
+
+console.log((Amazon * 4) + (Google * 6) + (Facebook *10));
+
+
+
+
+
+
+
+var classSizeMax = 25, classSize = 12, existingClassTime = 9, classTime = 11,
+    studentEnroll = classSize < classSizeMax && classTime !== existingClassTime;
+
+console.log(studentEnroll) ;
+
+
+
+
+
+
+
+var items = 7, couponExpired = false, validCoupon = items > 2 && couponExpired === false;
+
+console.log(validCoupon);
+
+
+// Exercise 4
+
+var username = 'codeup';
+var password = 'notastrongpassword';
+
+console.log(password.length > 5);
+console.log(!password.includes('codeup'));
+console.log(username.length < 20);
+console.log(!username.startsWith(" "));
+console.log(!username.endsWith(" "));
+console.log(!password.startsWith(" "));
+console.log(!password.endsWith(" "));
+
+
 
 
 
