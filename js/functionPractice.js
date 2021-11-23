bc7a60c1aebd34e901b3fdb161eda59a61b3730b
Andrew Lange <andrew.lange13@gmail.com>
2021-11-23 12:26:08 -0600

    moved to folder

---
+++ js/functionPractice.js
@@ -0,0 +1,42 @@
+// function isTodayAGoodDayToFly(isCloudy, planesFlying) {
+//     if (!isCloudy && planesFlying < 10) {
+//         console.log("It is a good day to fly!!!!");
+//     } else {
+//         console.log("It is not a good day to fly!!!");
+//     }
+//
+// }
+//
+// isTodayAGoodDayToFly(true, 4);
+
+
+function add(x, y) {
+    return (x + y);
+}
+
+console.log(add(4, 4));
+
+function subtractify(x, y) {
+    return (x - y);
+}
+
+console.log(subtractify(11, 8));
+
+function multiply(x, y) {
+    return (x * y);
+}
+
+console.log(multiply(7, 11));
+
+function divide(x, y) {
+    return (x / y);
+}
+
+
+console.log(divide(9, 3));
+
+function remainder(x, y) {
+    return (x % y);
+}
+
+console.log(remainder(7, 3));
