cb99cb002c7a5b858174eeae06bf2131d36f8903
Andrew Lange <andrew.lange13@gmail.com>
2021-11-30 12:03:07 -0600

    adds functionPractice

--- js/functionPractice.js
+++ js/functionPractice.js
@@ -10,7 +10,7 @@
 // isTodayAGoodDayToFly(true, 4);
 
 
-function add(x, y) {
+/*function add(x, y) {
     return (x + y);
 }
 
@@ -39,4 +39,90 @@ function remainder(x, y) {
     return (x % y);
 }
 
-console.log(remainder(7, 3));
+console.log(remainder(7, 3));*/
+
+
+function bowlingAverage(score1, score2, score3) {
+    return (score1 + score2 + score3) / 3;
+}
+
+console.log(bowlingAverage(180, 190, 210));
+
+////////
+
+let highScore = Math.max(190, 240, 110);
+
+
+function bowlingBestScore(n) {
+    if (n % 5 === 0 || n % 3 === 0) {
+        return "true"
+    } else {
+        return "false"
+    }
+}
+
+console.log(bowlingBestScore(highScore));
+
+/////
+
+function bowlingName(name) {
+    return name.toLowerCase();
+}
+
+console.log(bowlingName("ThEbEsTbOwLiNgPlAcEever"))
+
+/////
+
+let score = 109
+let name = "bowling Is Fun";
+let isBowlingFun = true;
+let players = ["tom", "jerry", "Garfield"]
+
+function bowlingType(score, name, isBowlingFun, players) {
+    if (score) {
+        return typeof score
+    } else if (name) {
+        return typeof name
+    } else if (isBowlingFun) {
+        return typeof isBowlingFun
+    } else if (players) {
+        return typeof players
+    }
+}
+
+console.log(bowlingType(players));
+
+
+////
+//
+// function executeAll(func1, func2, func3, func4) {
+//     func1();
+//     func2();
+//     func3();
+//     func4();
+// }
+//
+// console.log(bowlingAverage(),bowlingBestScore(),bowlingName(),bowlingType());
+
+////
+
+function myFunction(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+
+console.log(myFunction("Rob", "Robertson"));
+
+/////
+
+
+function thatsNotMyName(name) {
+    if (name === "Laura") {
+        return "that's me!, I'm " + name
+    } else {
+        return "that's not my name"
+    }
+}
+
+console.log(thatsNotMyName("Laura"))
+
+////
