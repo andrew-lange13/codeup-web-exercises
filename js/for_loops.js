d930b9da33b6047ac768d5059516b35c0a761788
Andrew Lange <andrew.lange13@gmail.com>
2021-12-01 18:20:09 -0600

    adds for_loops js and loops html files

---
+++ js/for_loops.js
@@ -0,0 +1,16 @@
+/// showMultiplicationTable
+
+function showMultiplicationTable(n){
+    console.log(n * 1);
+    console.log(n * 2);
+    console.log(n * 3);
+    console.log(n * 4);
+    console.log(n * 5);
+    console.log(n * 6);
+    console.log(n * 7);
+    console.log(n * 8);
+    console.log(n * 9);
+    console.log(n * 10);
+}
+
+showMultiplicationTable(1);
