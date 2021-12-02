c17264e4ed2cefd269075ae255d881a620f5c14a
Andrew Lange <andrew.lange13@gmail.com>
2021-12-01 19:09:13 -0600

    adds showMultiplicationTable to for_loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,16 +1,29 @@
-/// showMultiplicationTable
+'use strict'
+///// showMultiplicationTable
 
-function showMultiplicationTable(n){
-    console.log(n * 1);
-    console.log(n * 2);
-    console.log(n * 3);
-    console.log(n * 4);
-    console.log(n * 5);
-    console.log(n * 6);
-    console.log(n * 7);
-    console.log(n * 8);
-    console.log(n * 9);
-    console.log(n * 10);
+// function showMultiplicationTable(n) {
+//     console.log(n * x);
+//     console.log(n * 2);
+//     console.log(n * 3);
+//     console.log(n * 4);
+//     console.log(n * 5);
+//     console.log(n * 6);
+//     console.log(n * 7);
+//     console.log(n * 8);
+//     console.log(n * 9);
+//     console.log(n * 10);
+// }
+
+// showMultiplicationTable(7);
+
+
+for (let x = 1; x <= 10; x++) {
+    function showMultiplicationTable(n) {
+        return (n * x);
+    }
+
+    console.log(showMultiplicationTable(6))
 }
 
-showMultiplicationTable(1);
+////// forRandomBoolean
+
