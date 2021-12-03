9117bf64b1d72f82f6d87ad66c57264009c67ee7
Andrew Lange <andrew.lange13@gmail.com>
2021-12-03 16:48:19 -0600

    adds planets-string to exercise folder

--- js/planets-string.js
+++ js/planets-string.js
@@ -11,17 +11,29 @@
      * console.log planetsArray to check your work
      */
 
-    // console.log(planetsArray);
+    planetsArray = planetsString.split("|");
+
+    console.log(planetsArray);
 
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
      *
+     *
      * BONUS:
      * Create another string that would display your planets in an undordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+
+    var planetsString2 = "Mercury <br> Venus <br> Earth <br> Mars <br> Jupiter <br> Saturn <br> Uranus <br> Neptune";
+
+    console.log(planetsString2)
+
+    let htmlString = "<ul><li>Mercury</li><li>Venus</li><li>Earth</li><li>Mars</li><li>Jupiter</li><li>Saturn</li><li>Uranus</li><li>Neptune</li></ul>";
+
+    document.body.innerHTML = htmlString;
 
 })();
