f87da2d5b490e19d15269c463860e625b39ec9ee
Andrew Lange <andrew.lange13@gmail.com>
2021-12-03 09:45:40 -0600

    adds to for_loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -2,7 +2,7 @@
 ///// showMultiplicationTable
 
 // function showMultiplicationTable(n) {
-//     console.log(n * x);
+//     console.log(n * 1);
 //     console.log(n * 2);
 //     console.log(n * 3);
 //     console.log(n * 4);
@@ -13,22 +13,34 @@
 //     console.log(n * 9);
 //     console.log(n * 10);
 // }
-
-// showMultiplicationTable(7);
-
-
+// //
+// // showMultiplicationTable(7);
+//
+//
 // for (let x = 1; x <= 10; x++) {
 //     function showMultiplicationTable(n) {
 //         return (n * x);
 //     }
 //
-//     console.log(showMultiplicationTable(6))
+//     console.log(7 + " X " + x + " = " + showMultiplicationTable(7))
 // }
+
+//
+// function showMultiplicationTable(x){
+//     for(let i = 1; i <= 10; i++){
+//
+//         let answer = x * i;
+//
+//         console.log(x + " x " + i +  " = " + answer);
+//     }
+// }
+//
+// showMultiplicationTable(7);
 
 ////// randomEvenOdd
 
 // for (let i = 0; i < 10; i++) {
-//     let randomNum = Math.floor((Math.random() * (201 - 20)) + 1);
+//     let randomNum = Math.floor((Math.random() * 180) + 20);
 //
 //     function randomEvenOdd() {
 //         if (randomNum % 2 === 0) {
@@ -41,8 +53,8 @@
 //     console.log(randomEvenOdd());
 // }
 
-////// forWedge
-//
+//// forWedge
+
 // for (let i = 0; i < 10; i++) {
 //     let x = (i.toString()).repeat(i)
 //         console.log(x)
@@ -51,6 +63,11 @@
 
 ////// forSubtractFive
 
-for (let i = 100; i >= 5; i -= 5) {
-    console.log(i)
-}
+// for (let i = 100; i >= 5; i -= 5) {
+//     console.log(i)
+// }
+//
+
+//////
+
+
