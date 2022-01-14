8b35e532a0e7ec28db1f08278926bb63553911ee
Andrew Lange <andrew.lange13@gmail.com>
2022-01-14 09:19:52 -0600

    adds to jsPractice

--- jsPractice/loopPractice.js
+++ jsPractice/loopPractice.js
@@ -98,3 +98,11 @@
 //     }
 // }
 
+
+// Make a program that filters through this array: ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"] and returns a new array that contains only those whose name have only 4 letters in them.
+
+let namesArr = ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"];
+
+let answer = namesArr.filter(name => name.length === 4);
+
+console.log(answer);
