f7919ff723bb31bd62777a70d64ab7efd1558318
Andrew Lange <andrew.lange13@gmail.com>
2022-01-19 12:17:03 -0600

    adds jquery intro exercise

---
+++ js/jquery_exercises.js
@@ -0,0 +1,36 @@
+'use strict';
+
+$(document).ready(function (){
+    alert("The DOM has finished loading!");
+});
+
+
+
+// let val = $("#p-one").text();
+//
+// console.log(val)
+
+
+/// add a class of border to my p tag
+
+$("#p-one").addClass("border");
+
+// add heading to border is user is logged in
+// check value of isLoggedIn
+// compare value of logged in to true
+//add border
+///assign a class if true
+///find element
+///add class
+let user1 = {
+    name: "bob",
+    isLoggedIn: true,
+    age: 92,
+}
+
+let loginStatus = user1.isLoggedIn;
+
+if(loginStatus === true){
+    $("#heading").addClass("border");
+}
+
--- jsPractice/jsReview19Jan2022.js
+++ jsPractice/jsReview19Jan2022.js
@@ -4,48 +4,48 @@
 
 ////declaring an object
 
-let USA = {
-    Texas: {
-        size: "big",
-        flower: "bluebonnet",
-        bird: "mockingbird",
-        people: 30,
-        addPeople: function (){
-            this.people
-        }
-    }
-}
-
-
-let sirius = {
-    //properties
-    numStudents: 29,
-    city: "San Antonio",
-    inPerson: false,
-    theBest: true,
-
-    ////// methods - storage for functions
-    returnCity: function (){
-        return this.city
-    }
-
-}
-console.log(sirius.inPerson);  // dot notation
-console.log(sirius.returnCity());
-
-let cities = [
-    {city: "san antonio", people: 30, isGreat:true, team: "spurs"},
-    {city: "dallas", people: 40, isGreat:false, team: "mavs"},
-    {city: "houston", people: 50, isGreat:true, team: "rockets"},
-    {city: "austin", people: 20, isGreat:false, team: "none"},
-]
+// let USA = {
+//     Texas: {
+//         size: "big",
+//         flower: "bluebonnet",
+//         bird: "mockingbird",
+//         people: 30,
+//         addPeople: function (){
+//             this.people
+//         }
+//     }
+// }
+//
+//
+// let sirius = {
+//     //properties
+//     numStudents: 29,
+//     city: "San Antonio",
+//     inPerson: false,
+//     theBest: true,
+//
+//     ////// methods - storage for functions
+//     returnCity: function (){
+//         return this.city
+//     }
+//
+// }
+// console.log(sirius.inPerson);  // dot notation
+// console.log(sirius.returnCity());
+//
+// let cities = [
+//     {city: "san antonio", people: 30, isGreat:true, team: "spurs"},
+//     {city: "dallas", people: 40, isGreat:false, team: "mavs"},
+//     {city: "houston", people: 50, isGreat:true, team: "rockets"},
+//     {city: "austin", people: 20, isGreat:false, team: "none"},
+// ]
 
 //forEach
-cities.forEach(function (x){
-    console.log(x.city)
-})
-
-let sports = ["spurs", "mavericks", "rockets", "bucks"];
+// cities.forEach(function (x){
+//     console.log(x.city)
+// })
+//
+// let sports = ["spurs", "mavericks", "rockets", "bucks"];
 
 // sports.forEach(function (x){
 //     console.log("current team: " +x)
@@ -58,14 +58,15 @@ let sports = ["spurs", "mavericks", "rockets", "bucks"];
 // }
 
 /// does team name start with r?
+//
+// sports.forEach(function (team){
+//     let firstLetter = team.charAt(0)
+//     // console.log(firstLetter)
+//     if(firstLetter === "r"){
+//         console.log("true")
+//     }
+//     else{
+//         console.log("false")
+//     }
+// })
 
-sports.forEach(function (team){
-    let firstLetter = team.charAt(0)
-    // console.log(firstLetter)
-    if(firstLetter === "r"){
-        console.log("true")
-    }
-    else{
-        console.log("false")
-    }
-})
