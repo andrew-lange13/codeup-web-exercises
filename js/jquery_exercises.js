b21b33dd2714d38defd44b713cc91ebdf01c9a17
Andrew Lange <andrew.lange13@gmail.com>
2022-01-20 10:24:40 -0600

    adds jQuery mouse events exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -39,8 +39,24 @@
 
 // $(".codeup").css("border", "1px solid red");
 
-$("li").css("font-size", "20px");
-$("h1, p, li").css("background-color", "yellow");
-$("document").ready(function () {
-    alert($("h1").text())
+// $("li").css("font-size", "20px");
+// $("h1, p, li").css("background-color", "yellow");
+// $("document").ready(function () {
+//     alert($("h1").text())
+// })
+
+$("h1").click(function (){
+    $(this).css("background-color", "chartreuse")
+});
+
+
+$("p").dblclick(function (){
+    $(this).css("font-size", "18px")
+});
+
+$("li").hover(function () {
+    $(this).css("color", "red")
+}, function () {
+    $(this).css("color", "black")
 })
+
