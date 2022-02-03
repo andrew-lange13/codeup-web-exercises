04bac66cd2020e85371061ef9cbde55475481b63
Andrew Lange <andrew.lange13@gmail.com>
2022-02-03 09:23:29 -0600

    switches from bootstrap to mdb

--- js/weather_map.js
+++ js/weather_map.js
@@ -67,19 +67,21 @@ $(function () {
 
     });
 
-    map.on('load', function(){
-        map.addLayer({
-            "id": "simple-tiles",
-            "type": "raster",
-            "source": {
-                "type": "raster",
-                "tiles": ["https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=" + WEATHER_MAP_KEY],
-                "tileSize": 256
-            },
-            "minzoom": 0,
-            "maxzoom": 22
-        });
-    });
+    // Map weather layer
+
+    // map.on('load', function(){
+    //     map.addLayer({
+    //         "id": "simple-tiles",
+    //         "type": "raster",
+    //         "source": {
+    //             "type": "raster",
+    //             "tiles": ["https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=" + WEATHER_MAP_KEY],
+    //             "tileSize": 256
+    //         },
+    //         "minzoom": 0,
+    //         "maxzoom": 22
+    //     });
+    // });
 
 
 
