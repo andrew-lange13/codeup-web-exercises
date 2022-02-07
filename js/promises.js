fba8c8c6cd8728e4cc533ba85f096882f77b2f40
Andrew Lange <andrew.lange13@gmail.com>
2022-02-07 17:08:54 -0600

    adds to promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,10 +1,18 @@
-'use strict'
+'use strict';
 
-const gitCommit = () => fetch('https://api.github.com/repos/andrew-lange13/codeup-web-exercises/commits', {headers: {
-    'authorization': 'GITHUB'}})
-    .then(response => response.json());
+(function () {
+    const gitCommit = () => fetch('https://api.github.com/repos/andrew-lange13/codeup-web-exercises/commits', {headers: {
+            'authorization': 'GITHUB'}})
+        .then(response => response.json())
+        .then(repos => {
+            repos.forEach(repoObj => {
+                console.log(repoObj.commit.committer.date)
+            })
+        })
 
-console.log(gitCommit());
+
+    console.log(gitCommit());
+})();
 
 
 
