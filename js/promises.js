7e8834ea72e6c05e6790ec95c6b85c9bca7ce122
Andrew Lange <andrew.lange13@gmail.com>
2022-02-08 16:02:58 -0600

    adds to promises exercise

--- js/promises.js
+++ js/promises.js
@@ -8,14 +8,25 @@ $(function () {
             }
         }).then(response => response.json())
             .then(users => {
-                users.forEach(userObj => {
-                    console.log(userObj.created_at)
-                })
-            });
+                console.log(users[0]);
+                let lastCommit = `<p>${users[0].actor.login} Last Commit: ${users[0].created_at}</p>`
+                $(".container").html(lastCommit)
+            }).catch(err => console.log(err))
     }
 
 
     console.log(gitLastCommit("andrew-lange13"));
+
+    function wait(num) {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(`${num}`)
+            }, num)
+        })
+    }
+
+   wait(2000).then((num) => $(".container").append((`<p>You'll see this message after ${num/1000} seconds</p>`)))
+
 })();
 
 
