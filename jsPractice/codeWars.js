c57d859630ff8ffbd65d0068b7e5b3af54fa9289
Andrew Lange <andrew.lange13@gmail.com>
2022-04-09 18:04:28 -0500

    working on new codewars problem

--- jsPractice/codeWars.js
+++ jsPractice/codeWars.js
@@ -144,3 +144,9 @@ function warnTheSheep(queue) {
     }
 }
 
+// TODO: You receive an array with your peers' test scores. Now calculate the average and compare your score! Return True if you're better, else False!
+
+function betterThanAverage(classPoints, yourPoints) {
+
+}
+
