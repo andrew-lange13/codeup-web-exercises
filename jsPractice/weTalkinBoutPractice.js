03d78df2c4962a88840a07ddf0fa01762ed3e2d4
andrew-lange13 <andrew.lange13@gmail.com>
2022-08-03 11:34:58 -0700

    practice

--- jsPractice/weTalkinBoutPractice.js
+++ jsPractice/weTalkinBoutPractice.js
@@ -675,3 +675,23 @@ function howMuchTimeLeft(age){
 }
 
 howMuchTimeLeft(34);
+
+//The first parameter should be the weight and the second should be the height.
+
+
+
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8);
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+*/
+
+
+function bmiCalculator(weight, height){
+    return weight / (height * height);
+}
+
+console.log(bmiCalculator(99.8, 1.88));
