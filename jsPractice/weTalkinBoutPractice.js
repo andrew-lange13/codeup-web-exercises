3e88dced9cb4d333f4cdd3da944943b6c8c5af7c
andrew-lange13 <andrew.lange13@gmail.com>
2022-08-10 20:07:31 -0700

    project euler 1

--- jsPractice/weTalkinBoutPractice.js
+++ jsPractice/weTalkinBoutPractice.js
@@ -761,3 +761,22 @@ function fibonacciGenerator(n) {
 }
 
 console.log(fibonacciGenerator(26))
+
+
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+function sumOfMultiplesOf3And5(){
+    let sumOfMultiples = 0;
+    for(let i = 0; i<1000; i++){
+        if(i % 3 == 0 || i % 5 == 0){
+            sumOfMultiples +=i;
+        }
+    }
+    console.log(sumOfMultiples)
+}
+
+sumOfMultiplesOf3And5();
