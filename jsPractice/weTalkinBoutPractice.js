e5915843d8c9b7e66b72328bbb63e1e41d014f25
andrew-lange13 <andrew.lange13@gmail.com>
2022-08-12 10:12:28 -0700

    codewars

--- jsPractice/weTalkinBoutPractice.js
+++ jsPractice/weTalkinBoutPractice.js
@@ -806,3 +806,20 @@ function evenFibonacciSum(){
 }
 
 evenFibonacciSum();
+
+
+// In this kata, you are asked to square every digit of a number and concatenate them.
+//
+//     For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+
+function squareDigits(num){
+    let numArray = Array.from(num.toString()).map(Number);
+    let newNum = [];
+    for (let i = 0; i < numArray.length; i++){
+        newNum.push(numArray[i]*numArray[i])
+    }
+    return parseInt(newNum.join(""));
+}
+
+console.log(squareDigits(998));
